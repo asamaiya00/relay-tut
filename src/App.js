ab0070e045a2f3a7bc72c4dcbd2da3d15f4154af
asamaiya00 <asamaiya00@gmail.com>
2020-09-14 14:10:12 +0530

    fetchGraphQL function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
 
 function App() {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        repository(owner:"asamaiya00" name:"contact-manager"){
+          name
+        } 
+      } 
+    `)
+      .then((response) => {
+        if (!isMounted) {
+          return;
+        }
+
+        const data = response.data;
+        console.log(response)
+        setName(data.repository.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, [fetchGraphQL]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name ? `Repository: ${name}` : 'Loading'}</p>
       </header>
     </div>
   );
